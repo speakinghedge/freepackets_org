43b7f292e5a1805dee2fd26d497eff40b06b5563
speakinghedge <hecke@naberius.de>
2014-02-20 00:17:40 +0100

    remove tmp file

--- wrt54gl/firmware/OpenWrt-ImageBuilder-brcm47xx-for-Linux-i686/tmp/.host.mk
+++
@@ -1,8 +0,0 @@
-HOST_OS:=Linux
-HOST_ARCH:=x86_64
-GNU_HOST_NAME:=x86_64-linux-gnu
-TAR:=/bin/tar
-FIND:=/home/devel/projects/freepackets_org/wrt54gl/firmware/OpenWrt-ImageBuilder-brcm47xx-for-Linux-i686/staging_dir/host/bin/find
-BASH:=/bin/bash
-FIND_L=/home/devel/projects/freepackets_org/wrt54gl/firmware/OpenWrt-ImageBuilder-brcm47xx-for-Linux-i686/staging_dir/host/bin/find -L $(1)
-XARGS:=xargs -r
